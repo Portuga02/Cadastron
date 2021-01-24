03b07fed354fb6c9b6fea53d27b76365b0f5d973
Sávio gomes da Silva <saviogomesdasilva@yahoo.com>
2021-01-24 15:41:50 -0300

    configurando create

--- app/src/main/java/com/et/missae/cadastron/MainActivity.java
+++ app/src/main/java/com/et/missae/cadastron/MainActivity.java
@@ -1,14 +1,89 @@
-package com.et.missae.cadastron;
+    package com.et.missae.cadastron;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editNome, editTelefone, editEmail, editId;
+    Button btnNovo, btnSalvar, btnExcluir, btnEditar;
+    ListView listViewContatos;
+
+    private final String HostName = "https://192.168.1.11/contatos";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editNome =(EditText) findViewById(R.id.editNome);
+
+        editTelefone =(EditText) findViewById(R.id.editTelefone);
+
+        editEmail =(EditText) findViewById(R.id.editEmail);
+
+        editId =(EditText) findViewById(R.id.editId);
+
+        btnNovo = (Button) findViewById(R.id.btnNovo);
+
+        btnSalvar = (Button) findViewById(R.id.btnSalvar);
+       btnExcluir = (Button) findViewById(R.id.btnExcluir);
+
+        btnEditar=(Button) findViewById(R.id.btnEditar);
+
+        listViewContatos = (ListView)  findViewById(R.id.listViewContatos);
+
+        btnSalvar.setOnClickListener(v -> {
+            String id = editId.getText().toString();
+            String nome = editNome.getText().toString();
+            String email = editEmail.getText().toString();
+            String telefone = editTelefone.getText().toString();
+
+            String url = HostName + "/create.php";
+            if (nome.isEmpty()){
+                editNome.setError("O nome está em branco por favor digite um nome no campo");
+            } else if(id.isEmpty()){
+                
+                Ion.with(MainActivity.this)
+                        .load(url)
+                        .setBodyParameter("nome", nome)
+                        .setBodyParameter( "telefone", telefone)
+                        .setBodyParameter("email", email)
+                        .asJsonObject()
+                        .setCallback(new FutureCallback<JsonObject>() {
+                            @Override
+                            public void onCompleted(Exception e, JsonObject result) {
+                                
+                                if(result.get("CREATE").getAsString().equals("OK")){
+                                    
+                                    int IdRetorno = Integer.parseInt(result.get("ID").getAsString());
+                                    
+                                    Toast.makeText(MainActivity.this,"Salvo com sucesso" + IdRetorno,
+                                            Toast.LENGTH_LONG).show();
+
+
+                                } else{
+                                    Toast.makeText(MainActivity.this,"Ocorreu Algum erro ao salvar",Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+            } else{
+                //UPDATE
+            }
+        });
     }
 }
