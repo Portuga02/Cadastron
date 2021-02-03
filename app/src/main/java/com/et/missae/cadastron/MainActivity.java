78467a839f885dac818e4d63a26d8be825cde534
Sávio gomes da Silva <saviogomesdasilva@yahoo.com>
2021-02-02 22:47:29 -0300

    configurando bloco try

--- app/src/main/java/com/et/missae/cadastron/MainActivity.java
+++ app/src/main/java/com/et/missae/cadastron/MainActivity.java
@@ -1,18 +1,19 @@
-    package com.et.missae.cadastron;
+package com.et.missae.cadastron;
 
-    import android.os.Build;
-    import android.os.Bundle;
-    import android.widget.Button;
-    import android.widget.EditText;
-    import android.widget.ListView;
-    import android.widget.Toast;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-    import androidx.annotation.RequiresApi;
-    import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 
-    import com.google.gson.JsonObject;
-    import com.koushikdutta.async.future.FutureCallback;
-    import com.koushikdutta.ion.Ion;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,17 +30,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editNome =(EditText) findViewById(R.id.editNome);
-        editTelefone =(EditText) findViewById(R.id.editTelefone);
-        editEmail =(EditText) findViewById(R.id.editEmail);
-        editId =(EditText) findViewById(R.id.editId);
+        editNome = (EditText) findViewById(R.id.editNome);
+        editTelefone = (EditText) findViewById(R.id.editTelefone);
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editId = (EditText) findViewById(R.id.editId);
 
         btnNovo = (Button) findViewById(R.id.btnNovo);
         btnSalvar = (Button) findViewById(R.id.btnSalvar);
         btnExcluir = (Button) findViewById(R.id.btnExcluir);
-        btnEditar=(Button) findViewById(R.id.btnEditar);
+        btnEditar = (Button) findViewById(R.id.btnEditar);
 
-        listViewContatos = (ListView)  findViewById(R.id.listViewContatos);
+        listViewContatos = (ListView) findViewById(R.id.listViewContatos);
 
         btnSalvar.setOnClickListener(v -> {
             String id = editId.getText().toString();
@@ -47,35 +48,41 @@ public class MainActivity extends AppCompatActivity {
             String email = editEmail.getText().toString();
             String telefone = editTelefone.getText().toString();
 
-            String url = HOST+"/create.php";
-            if (nome.isEmpty()){
+            String url = HOST + "/create.php";
+            if (nome.isEmpty()) {
                 editNome.setError("O nome está em branco por favor digite um nome no campo");
-            } else if(id.isEmpty()){
+            } else if (id.isEmpty()) {
 
                 Ion.with(MainActivity.this)
                         .load(url)
                         .setBodyParameter("nome", nome)
-                        .setBodyParameter( "telefone", telefone)
+                        .setBodyParameter("telefone", telefone)
                         .setBodyParameter("email", email)
                         .asJsonObject()
-                        .setCallback(( new FutureCallback<JsonObject>() {
+                        .setCallback((new FutureCallback<JsonObject>() {
 
                             @Override
                             public void onCompleted(Exception e, JsonObject result) {
-                                if(result.get("CREATE").getAsString().equals("OK")){
-                                    int idRetornado = Integer.parseInt(result.get("ID").getAsString());
-                                    Toast.makeText(MainActivity.this,
-                                            "Salvo com sucesso, id" + idRetornado,
-                                            Toast.LENGTH_LONG).show();
-                                } else{
-                                    Toast.makeText(MainActivity.this,
-                                            "Ocorreu um erro ao salvar" ,
-                                            Toast.LENGTH_LONG).show();
+                                try {
+                                    if (result.get("CREATE").getAsString().equals("OK")) {
+                                        int idRetornado = Integer.parseInt(result.get("ID").getAsString());
+                                        Toast.makeText(MainActivity.this,
+                                                "O CONTATO FOI SALVO COM SUCESSO NO ID(" + idRetornado + ")",
+                                                Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(MainActivity.this,
+                                                "Ocorreu um erro ao salvar",
+                                                Toast.LENGTH_LONG).show();
+
+                                    }
+                                } catch (OutOfMemoryError e1) {
+                                    e1.printStackTrace();
+                                    Log.e("Memory exceptions", "exceptions" + e1);
 
                                 }
                             }
                         }));
-            } else{
+            } else {
 
             }
         });
